0c107977a995f9a27cb7a684e4cb00e9ef25b51d
方积极 <565205645@qq.com>
2023-06-08 21:01:43 +0800

    fwl 上传app和main，router，store基础文件

---
+++ database/src/main.js
@@ -0,0 +1,24 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dialogPage from '@/components/dialogPage'
+import messagePage from '@/components/messagePage'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+
+
+app
+.component('dialogPage',dialogPage)
+.component('messagePage',messagePage )
+.use(store)
+.use(router)
+.use(ElementPlus)
+.mount('#app')
---
+++ database/src/store/index.js
@@ -0,0 +1,18 @@
+
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    control:{
+  }
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+    
+  },
+  modules: {
+  }
+})

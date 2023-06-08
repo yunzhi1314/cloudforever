ca97545b538707d08efd1c2190ed8c6be8f8cfdd
liberfly123 <360383409@qq.com>
2023-06-08 11:16:37 +0800

    清风化化更改2

--- its.js
+++ its.js
@@ -1,4 +1,10 @@
 function sayHi(){
     console.log(1111)
     console.log('aaav')
+    console.log("asdawfa")
+}
+function sayHi2(){
+    console.log(1111)
+    console.log('aaav')
+    console.log("asdawfa")
 }

1d20b0f4b57fa5af0adf3763cb0ac76ac1cc93ec
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-09 12:05:25 +0800

    ttteest

--- arknights/arkknigt/src/store/index.js
+++ arknights/arkknigt/src/store/index.js
@@ -1,7 +1,12 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 export default createStore({
+  plugins: [createPersistedState({
+    storage: window.sessionStorage
+  })],
   state: {
+    isDataBase: false,
   },
   getters: {
   },

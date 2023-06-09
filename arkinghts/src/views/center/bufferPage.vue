555c2e04e2bb350dcbbcc7c4be7a8433d7af59b6
yxk <3126568631@qq.com>
2023-06-09 10:51:41 +0800

    new file:   arkinghts/src/views/center/bufferPage.vue 	new file:   arkinghts/src/views/center/centerPage.vue 	new file:   arkinghts/src/views/center/loginPage.vue

---
+++ arkinghts/src/views/center/bufferPage.vue
@@ -0,0 +1,17 @@
+<template>
+                <h1>buffer组件</h1>
+            </template>
+
+            <script>
+
+            export default {
+              name:'bufferPage',
+                setup() {
+                    return {
+                    }
+                }
+            }
+            </script>
+
+            <style lang="scss" scoped>
+            </style>

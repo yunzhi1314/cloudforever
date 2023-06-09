7bb323a03c3f4d373151dd0cb5a5e83d6c73856f
yxk <3126568631@qq.com>
2023-06-09 10:51:41 +0800

    new file:   arkinghts/src/views/center/bufferPage.vue 	new file:   arkinghts/src/views/center/centerPage.vue 	new file:   arkinghts/src/views/center/loginPage.vue

--- arkinghts/src/router/index.js
+++ arkinghts/src/router/index.js
@@ -1,7 +1,19 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-   {}
+   {
+    path:'/',
+    name:'centerPage',
+    component:()=>import('@/views/center/centerPage'),
+    children:[
+      {
+        path:'/login',
+        name:'loginPage',
+        component:()=>import('@/views/center/loginPage')
+      }
+    ]
+   },
+  
 ]
 
 const router = createRouter({
---
+++ arkinghts/src/views/center/centerPage.vue
@@ -0,0 +1,17 @@
+<template>
+                <h1>centerPage组件</h1>
+            </template>
+
+            <script>
+
+            export default {
+              name:'centerPage',
+                setup() {
+                    return {
+                    }
+                }
+            }
+            </script>
+
+            <style lang="scss" scoped>
+            </style>

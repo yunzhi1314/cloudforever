df6926b1f3fd1424c1faefb066fab48eb991b2f7
啊啊啊啊 <1302853532@qq.com>
2023-06-09 13:22:47 +0800

    zxy修改了文件app.vue,center.scss,centerPage.vue

--- arkinghts/src/views/center/centerPage.vue
+++ arkinghts/src/views/center/centerPage.vue
@@ -1,19 +1,44 @@
 <template>
-    <div>
-        <header></header>
+    <div class="center">
+        <header>
+            <div></div>
+            <div></div>
+            <div>
+              <section>
+                <p>
+                  <!-- <img src="" alt=""> -->
+                  客服中心
+                </p>
+                <p>客服中心</p>
+              </section>
+            </div>
+        </header>
         <main>
+          <div></div>
           <router-view></router-view>
         </main>
+        <footer>
+          <section>
+            <img src="" alt="">
+            <span></span>
+          </section>
+        </footer>
     </div>
-</template>
+</template> 
 
 <script>
+import centerSCSS from "@/public/center.scss"
+
 export default {
   name: "centerPage",
   setup() {
-    return {};
+    return {
+      centerSCSS
+    };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>

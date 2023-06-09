2a0e7979dd27eebff1422b2383d2bf0d6639bab7
fatayang <1426122011@qq.com>
2023-06-09 13:41:31 +0800

    new file:   Aknight/src/hooks/personalCenter/changeBoard.js

--- Aknight/src/views/center/login/loginPage.vue
+++ Aknight/src/views/center/login/loginPage.vue
@@ -5,15 +5,20 @@
 		<div v-for="(item, index) in loginArr" :key="index">
 			<!-- 输入框区 -->
 			<section>
-				<input v-model="item.value" :type="item.type" :placeholder="item.placeHolder"
+				<input
+					v-model="item.value"
+					:type="item.type"
+					:placeholder="item.placeHolder"
 					:style="{ backgroundColor: item.isShow ? '#FFDCDB' : '' }" />
 			</section>
 			<!-- 错误提示区 -->
-			<section class="errMsg" :style="{
-				visibility: item.isShow ? 'visible' : 'hidden',
-				fontSize: '12px',
-				color: '#f00'
-			}">
+			<section
+				class="errMsg"
+				:style="{
+					visibility: item.isShow ? 'visible' : 'hidden',
+					fontSize: '12px',
+					color: '#f00'
+				}">
 				{{ item.value == "" ? item.tip01 : item.tip02 }}
 			</section>
 		</div>
@@ -28,43 +33,52 @@
 	</div>
 	<!-- 切换登录注册框 -->
 	<div class="change-box">
-		<span :class="controlObj.isChange ? '' : 'checked'">登录</span>
+		<span
+			:class="controlObj.isChange ? '' : 'checked'"
+			@click="changeBoard(loginArr, controlObj, false, loginArr.length > 3)"
+			>登录</span
+		>
 		<span>·</span>
-		<span :class="controlObj.isChange ? 'checked' : ''">注册</span>
+		<span
+			:class="controlObj.isChange ? 'checked' : ''"
+			@click="changeBoard(loginArr, controlObj, true,loginArr.length < 3,loginArr)"
+			>注册</span
+		>
 	</div>
 </template>
 
 <script>
-import controlObj from "@/hooks/personalCenter/controlObj";
-import { reactive } from "vue";
-import { observer } from "@/hooks/personalCenter/watcher";
-export default {
-	name: "loginPage",
-	setup() {
-		let loginArr = reactive([
-			{
-				value: "",
-				isShow: false,
-				type: "tel",
-				placeHolder: "请输入手机号",
-				tip01: "*手机号码不能为空",
-				tip02: "*手机号码格式不正确",
-				zz: /^1{1}[3-9]{1}\d{9}$/
-			},
-			{
-				value: "",
-				isShow: false,
-				type: "password",
-				placeHolder: "请输入密码",
-				tip01: "*密码不能为空",
-				tip02: "*密码格式不正确",
-				zz: /^\w{8,16}$/
-			}
-		]);
-		observer(loginArr)
-		return { loginArr, controlObj };
-	}
-};
+	import controlObj from "@/hooks/personalCenter/controlObj";
+	import { reactive } from "vue";
+	import { observer } from "@/hooks/personalCenter/watcher";
+	import { changeBoard } from "@/hooks/personalCenter/changeBoard";
+	export default {
+		name: "loginPage",
+		setup() {
+			let loginArr = reactive([
+				{
+					value: "",
+					isShow: false,
+					type: "tel",
+					placeHolder: "请输入手机号",
+					tip01: "*手机号码不能为空",
+					tip02: "*手机号码格式不正确",
+					zz: /^1{1}[3-9]{1}\d{9}$/
+				},
+				{
+					value: "",
+					isShow: false,
+					type: "password",
+					placeHolder: "请输入密码",
+					tip01: "*密码不能为空",
+					tip02: "*密码格式不正确",
+					zz: /^\w{8,16}$/
+				}
+			]);
+			observer(loginArr);
+			return { loginArr, controlObj, changeBoard };
+		}
+	};
 </script>
 
 <style lang="scss" scoped></style>

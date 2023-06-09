d0e474507517f80a141dac6da64537206b74f233
zengjunl <545385447@qq.com>
2023-06-09 14:27:09 +0800

    zjl增加了吐丝，修改了main.js

---
+++ Aknight/src/components/messagePage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div> 
+        <section class="tusi" :style='{
+        backgroundColor:status == 1? "red" : "green"
+    }'>
+
+        </section>  
+
+    </div>
+</template>
+
+<script>
+import controlObj from '@/hooks/personalCenter/controlObj'
+export default {
+    name:"messagePage",
+    setup () {
+        
+
+        return {
+            controlObj,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.tusi{
+    width: 200px;
+    height: 50px;
+    background-color: green;
+    border-radius: 1vw;
+    position: absolute;
+    left: 43vw;
+    top: 12vh;
+    color: #fff;
+    text-align: center;
+    line-height: 50px; 
+
+}
+
+</style>
---
+++ Aknight/src/hooks/toset.js
@@ -0,0 +1,8 @@
+// 封装吐丝
+export function toest(){
+    controlObj.isTusi = true
+
+    setTimeout(() => {
+        controlObj.isTusi = false
+    }, 2000)
+}

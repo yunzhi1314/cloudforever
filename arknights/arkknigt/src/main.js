5a566eea9f301b68bf27d58c1e920242a1861e8d
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-09 13:15:25 +0800

    添加element-plus

--- arknights/arkknigt/src/main.js
+++ arknights/arkknigt/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+//element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(ElementPlus).use(store).use(router).mount('#app')

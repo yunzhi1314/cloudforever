894cbe9623adec8c0a7bccea2cfd031d4cd8e10d
sdf168 <766703981@qq.com>
2023-06-09 20:37:44 +0800

    添加addmenu文件

---
+++ database/src/api/addMenu.js
@@ -0,0 +1,13 @@
+import  {Request} from "@/hooks/Requset";
+import url from "./url";
+import { reactive } from "vue";
+
+
+// 新增菜单请求
+export function addMenu(){
+    let dataList=reactive({
+        list:[]
+    })
+    
+    
+}

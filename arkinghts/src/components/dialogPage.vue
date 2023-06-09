d3a7519dbe8aa08e75b34370f051a338e90aba00
yxk <3126568631@qq.com>
2023-06-09 19:11:41 +0800

    modified:   arkinghts/src/components/dialogPage.vue

--- arkinghts/src/components/dialogPage.vue
+++ arkinghts/src/components/dialogPage.vue
@@ -1,12 +1,12 @@
 <template>
     <teleport to='body'>
-        <div class="dialog" v-show="controlObj.isDialog[controlDialog]" @click="cancel">
-               
+        <div class="dialog" v-show="controlObj.isDialog[controlDialog]" @click="cancel">   
         </div>
     </teleport>
 </template>
 
 <script>
+
 export default {
     setup () {
         
@@ -21,6 +21,13 @@ export default {
     width: 100vw;
     height: 100vh;
     background-color: rgba(255,255,255,0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
    } 
   
 

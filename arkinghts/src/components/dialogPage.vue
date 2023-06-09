55ef9f41f894e6a9b4f324833b5d3745ef05a7cd
yxk <3126568631@qq.com>
2023-06-09 14:31:38 +0800

    new file:   arkinghts/src/components/dialogPage.vue

---
+++ arkinghts/src/components/dialogPage.vue
@@ -0,0 +1,27 @@
+<template>
+    <teleport to='body'>
+        <div class="dialog" v-show="controlObj.isDialog[controlDialog]" @click="cancel">
+               
+        </div>
+    </teleport>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+   .dialog{
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(255,255,255,0.3);
+   } 
+  
+
+</style>

decfcc56e7b4550b272afcedb6138e3de20e4b47
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-09 10:23:08 +0800

    删除源文件添加url

---
+++ arknights/arkknigt/src/api/url.js
@@ -0,0 +1,27 @@
+export default {
+    //数据库url
+    database: {
+        layout: {
+            menu: "/database/layout/menu"
+        },
+        home: {
+            messages: "/database/home/messages",
+            expMedicals: "/database/home/expMedicals",
+            basicMedical: "/database/home/basicMedical",
+            navTools: "/database/home/navTools",
+            naweiCompany: "/database/home/naweiCompany",
+        },
+        menu: {
+            addMenu: "/database/menu/addMedical",
+            setMenu: "/database/menu/setMedical",
+            delMenu: "/database/menu/delMedical"
+        },
+        role: {
+            addMenu: "/database/role/addMedical",
+            delMenu: "/database/role/delMedical"
+        },
+        user: {
+            queryMedicals: "database/home/expMedical"
+        }
+    }
+}
---
+++ arknights/arkknigt/src/views/dataBase.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'database',
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

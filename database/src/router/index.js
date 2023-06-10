828c30c648cceeea2b57524b13e268f71a0701f3
leihonggege <1479960461@qq.com>
2023-06-10 16:41:10 +0800

    ljs 增加了了router文件的路由守卫

--- database/src/router/index.js
+++ database/src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { dealRoutes } from "@/hooks/route";
 
 const routes = [
 	{
-		path:'/',
-		name:'database',
-		component:()=>import('@/views/database/databasePage.vue'),
-		children:[
+		path: '/',
+		name: 'database',
+		component: () => import('@/views/database/databasePage.vue'),
+		children: [
 			{
 				path: "home",
 				name: "homePage",
 				component: () => import("@/views/database/home/homePage"),
 			},
 			{
-				path:"menu",
-				name:"menuPage",
-				component:()=>import("@/views/database/system/menu/menuPage")
+				path: "menu",
+				name: "menuPage",
+				component: () => import("@/views/database/system/menu/menuPage")
 			}
 		]
 	},
@@ -25,5 +26,39 @@ const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
 	routes,
 });
+
+let whiteNames = ['/']
+
+router.beforeEach((to, from, next) => {
+	if (whiteNames.includes(to.path)) {
+		//放行
+		next()
+	} else {
+		if (JSON.parse(localStorage.getItem('token'))) {
+			let token = JSON.parse(localStorage.getItem("token")).token
+			if (token) {
+				// mathch值有的话就放行，防止页面刷新后空白
+				//其他页面刷新返回首页
+				if (to.matched[0]) {
+					next()
+				} else {			
+					let routeData = JSON.parse(sessionStorage.getItem("menuList"))
+					let route = Reflect.get(routeData, 'menuList')
+					route = dealRoutes(route, route.length - 1)
+
+					route.forEach(item => {
+						router.addRoute("menuList", item)
+					})
+					next({ ...to, replace: true })
+				}
+			}
+
+		} else {
+			//无权限放回指定路径
+			next({ path: "/" })
+		}
+	}
+})
+
 
 export default router;

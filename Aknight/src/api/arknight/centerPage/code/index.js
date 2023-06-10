3e72e0d9151a4c07d86c5d7ba48d55be4f3a2c2c
junMling <847233164@qq.com>
2023-06-10 14:57:22 +0800

    jml修改了登录注册以及发送短信的api,完善了部分注释说明

--- Aknight/src/api/arknight/centerPage/code/index.js
+++ Aknight/src/api/arknight/centerPage/code/index.js
@@ -16,7 +16,7 @@ export function mathCode() {
 }
 
 //发送短信以及注册登录通用
-//url:对应请求路径 data:请求所需要传的数据 changeFn:vuex中对应的mutation方法名字
+//url:对应请求路径 data:请求所需要传的数据 changeFn:vuex中对应的mutation方法名字,此处是vuex存吐丝的方法名
 //fn:用于区分是调用的根仓库还是子仓库的方法
 //如果是子仓库方法就需要用bind改变仓库的this指向,根仓库则不需要 =>根仓库本身指向于设置的store,子仓库指向于vuex全局的store
 //isStore:对应存储开关
@@ -28,8 +28,9 @@ export function user(url, data, changeFn, fn, isStore) {
     Request.postData(url, data).then((res) => {
         dataList.datas = toRef({ ...res.data });
         if (fn) {//代表存在调用的是子仓库函数
-            //改变指向
+            //改变指向并调用对应子仓库函数
             fn.bind({ $store: store })(dataList.datas)
+
             //开启存储开关(登录注册需要)
             store.commit("changeStore", isStore)
             //调用吐丝

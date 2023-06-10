2e5a26e976e1258319c2a91c733e8669b32fff05
xiaomo <183623313@qq.com>
2023-06-10 11:47:51 +0800

    在api里面创建一个database文件夹，里面再生成一个index.js,写了一个请求文件

--- arknights/arkknigt/src/api/database/home/index.js
+++ arknights/arkknigt/src/api/database/home/index.js
@@ -1,17 +1,3 @@
-import { onBeforeMount,reactive } from "vue";
-import { Request } from "@/hook/request";
-
-export function homeData(url){
-    let dataList = reactive({
-        homedata:[],
-    })
-
-    onBeforeMount(() => {
-        Request.get(url)
-        .then((res) =>{
-            dataList.homedata = {...res.data.menu};
-        })
-    })
 
     // 抛出数据
     return dataList

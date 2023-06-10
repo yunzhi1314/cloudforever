00a1eb349a4bbdab234f6456036b6a87a3d4fda7
junMling <847233164@qq.com>
2023-06-10 10:55:38 +0800

    jml修改了一下刚才合并之后的一点小错误(代码重复)

--- Aknight/src/hooks/personalCenter/code.js
+++ Aknight/src/hooks/personalCenter/code.js
@@ -33,8 +33,6 @@ export function setCountDown(name, arr) {
         //倒计时数字
         numberLogin: store.state.countDown,
         numberRegister: store.state.countDown,
-        numberLogin: store.state.countDown,
-        numberRegister: store.state.countDown,
         //倒计时开关
         isOpen: false,
         //倒计时定时器集合

d2619124173cb97e933717cb3a32d2f261f63229
yxk <3126568631@qq.com>
2023-06-10 11:06:36 +0800

    modified:   arkinghts/src/views/center/loginPage.vue

--- arkinghts/src/views/center/loginPage.vue
+++ arkinghts/src/views/center/loginPage.vue
@@ -80,15 +80,15 @@
 import loginCSS from "@/public/login.scss";
 import { reactive } from "vue";
 import { watcher } from "@/hooks/personalCenter/watcher"; //监视函数
-import controlsObj from "@/hooks/personalCenter/controls";
-import { getMathcode } from "@/hooks/personalCenter/code"
-
+import controlObj from "@/hooks/personalCenter/control";
+import { getMathCode, againGetMathCode,cancel } from "@/hooks/personalCenter/code"
+import svg from '@/hooks/personalCenter/code'
 export default {
   name: "loginPage",
   setup() {
     // 登录数组
     let loginArr = reactive([
-      {
+    {
         value: "",
         isShow: false,
         tip: "*账号格式不正确",
@@ -96,6 +96,7 @@ export default {
         type: "tel",
         placeholder: "请输入手机号",
         zz: /^1{1}[3-9]{1}\d{9}$/,
+        use:"手机号"
       },
       {
         value: "",
@@ -105,17 +106,19 @@ export default {
         type: "password",
         placeholder: "8-16位数字、字母、常用字符",
         zz: /^\w{8,16}$/,
+        use:"密码"
       },
     ]);
     // 注册页面增加数组
     let newArr = reactive([
-      {
+    {
         value: "",
         isShow: false,
         tip: "*请确认密码",
         tip1: "*两次输入的密码不一致",
         type: "password",
         placeholder: "请确认密码",
+        use:"确认密码",
       },
       {
         value: "",
@@ -126,6 +129,7 @@ export default {
         placeholder: "请输入验证码",
         zz: /^\d{4}$/,
         isCode: true,
+        use:"验证码"
       },
     ]);
 
@@ -147,22 +151,31 @@ export default {
     provide("controlDialog", "isMathCode");
 
 
+
+     // 短信验证码需传送的数据
+     let useInfo = reactive({
+        telephone:"",
+        mathCode:""
+    })
+    // 点击遮罩层确认按钮，请求短信验证码，并且关闭遮罩层
+   function confirm(name){
+    controlObj.isDialog[name] = false
+    let obj = loginArr.find(item => item.use == "手机号")
+    useInfo.telephone = obj.value
+    telCode(useInfo)
+
+}
+
     return {
       loginCSS,
       // 登录数组渲染
       loginArr,
-
-      // 注册增加数组
-      newArr,
-      // 点击去往注册框
-      changeRegister,
-      // 点击去往登录框
-      changeLogin,
-      // 全局开关对象
-      controlsObj,
-      changeRegister,
-      getMathcode,
-
+      changeRegister, //点击去注册页面的按钮
+      changeLogin, //点击去登录页面的按钮
+      controlObj, //页面控制状态的按钮
+      getMathCode, //获取图形验证码函数
+      svg,//图形验证码svg工具
+      againGetMathCode,//点击svg图片再次发起请求，更新图形验证码
     };
   },
 };

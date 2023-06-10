50b606ee3a3d5170a8a8a4cfdef5e33d0ed5a15d
yxk <3126568631@qq.com>
2023-06-10 18:06:04 +0800

    modified:   arkinghts/src/hooks/personalCenter/watcher.js 	modified:   arkinghts/src/router/index.js 	modified:   arkinghts/src/store/index.js 	modified:   arkinghts/src/store/module/personalCenter.js 	modified:   arkinghts/src/views/center/loginPage.vue

--- arkinghts/src/router/index.js
+++ arkinghts/src/router/index.js
@@ -1,24 +1,56 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-   {
-    path:'/center',
-    name:'centerPage',
-    component:()=>import('@/views/center/centerPage.vue'),
-    children:[
+  {
+    path: "/center",
+    name: "centerPage",
+    component: () => import("@/views/center/centerPage.vue"),
+    children: [
       {
-        path:'/center/login',
-        name:'loginPage',
-        component:()=>import('@/views/center/loginPage.vue')
-      }
-    ]
-   },
-  
-]
+        path: "/center/login",
+        name: "loginPage",
+        component: () => import("@/views/center/loginPage.vue"),
+      },
+      {
+        path: "/center/buffer",
+        name: "bufferPage",
+        component: () => import("@/views/center/bufferPage.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+// 定义白名单
+const whiteNames = ["/center", "/center/login"];
+// 全局路由守卫beforeEach
+router.beforeEach((to, from, next) => {
+  // 将所要跳转的页面路径与白名单一一对比
+  if (whiteNames.includes(to.path)) {
+    // 白名单内的网页路径，直接放行
+    next();
+  } else {
+    // 需要权限的页面
+    if (JSON.parse(sessionStorage.getItem("token"))) {
+      let token = JSON.parse(sessionStorage.getItem("token")).token;
+      // 有权限就放行
+      if (token) {
+        if (to.matched[0]) {
+          next();
+        } else {
+          // 重复导航，直到路由能够找到正确的路径为止
+          next({ ...to, replace: true });
+        }
+      }
+    } else {
+      // 无权限回转登录页面
+      next({ path: "/center/login" });
+    }
+  }
+});
+
+export default router;

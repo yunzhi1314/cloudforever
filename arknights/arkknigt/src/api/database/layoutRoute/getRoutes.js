79f9adceafb89cf8cf4f6cdcb71f7c9f9f2638b6
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-10 17:34:23 +0800

    添加说明

--- arknights/arkknigt/src/api/database/layoutRoute/getRoutes.js
+++ arknights/arkknigt/src/api/database/layoutRoute/getRoutes.js
@@ -9,7 +9,7 @@ export function layoutRoutes() {
         menus: []
     })
     Request.get('http://192.168.2.26:3000/api/normalData').then(res => {
-        dataList.menus = { ...res.data }
+        dataList.menus = { ...res.data } //路由菜单
         let a = dealpaths(res.data, 0, "component")
         store.commit('dataStore/SAVE_ROUTES', { a, memuList: dataList.menus }) //保存到仓库
         store.commit("CHANGE_STORE", "isDataBase") //开启保存仓库

e2a4580fe86d9bbf3ab5c733cac101376b0c6fd2
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-10 16:06:41 +0800

    将获取到的echarts图表数据渲染到页面上

--- arknights/arkknigt/src/views/database/home/homePage.vue
+++ arknights/arkknigt/src/views/database/home/homePage.vue
@@ -1,5 +1,15 @@
 <template>
-    <!-- 统计图 -->
+    <!-- 饼状统计图 -->
+    <div id="homePage">
+        <div v-for="(a,b) in homedata" :key="b">
+            <div>
+                <section>
+                    <span></span>
+                </section>
+            </div>
+        </div>
+    </div>
+    <!-- 折现统计图 -->
     <div class="plans" 
     style="width: 56vw;
      height: 50vh; 
@@ -15,27 +25,20 @@
 </template>
 
 <script>
-// import url from "@/api/url";
-
-// import { toRefs } from "vue";
-
-import { exMpedicals } from "@/echarts/index";
-
-import { onMounted } from "vue";
-
+import { ExpMedical } from "@/echarts/echarts";
+import { onMounted, toRefs } from "vue";
+import {homeData} from '@/api/database/home/index.js'
 export default {
     name:"homePage",
     setup () {
         onMounted(() => {
-            // 靶向药实验数据统计图
-            exMpedicals(document.querySelector("#exMpedicals"));
-
-            // 各公司所持有的靶向药数据，环状图
-            // basicMedicals(document.querySelector("#basicMedicals"));
+            // 折线图
+            ExpMedical(document.querySelector("#exMpedicals"));
 
         })
+        console.log(Math.sqrt(2,10))
         return {
-           
+           ...toRefs(homeData('http://192.168.2.26:3000/api/message'))
         }
     }
 }

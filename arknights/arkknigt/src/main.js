508f3d2fcae79c33babcd067c7b6ed4f67405997
yzy <1375223150@qq.com>
2023-06-10 09:06:16 +0800

    YZY 添加了UI的表格部分　

--- arknights/arkknigt/src/main.js
+++ arknights/arkknigt/src/main.js
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//element-plus
-import ElementPlus from 'element-plus'
+// 引入element-plus
+import ElementPlus from "element-plus"
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+
+for(const[key,component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key,component)
 }
+
 app.use(ElementPlus).use(store).use(router).mount('#app')

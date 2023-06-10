5f82b16374fd6eb77d73247361c2a682db07c320
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-10 16:05:58 +0800

    增加了保存获取图表数据的仓库方法

--- arknights/arkknigt/src/store/index.js
+++ arknights/arkknigt/src/store/index.js
@@ -13,21 +13,33 @@ export default createStore({
       return isStore(state, "saveRoutes", "isDataBase", routesSave, sessionStorage)
     },
     key: 'saveRoutes'
-  })],
+  }),
+  createPersistedState({
+    storage: window.sessionStorage,
+    reducer(state) {
+      let expMedicalData = {
+        expMedical: state.dataStore.expMedical
+      }
+      return isStore(state, "expMedical", "isExpMedical", expMedicalData, sessionStorage)
+    },
+    key: 'expMedical'
+  }),
+
+  ],
   state: {
     control: {
       isDataBase: false,
+      isExpMedical:false,
     }
   },
   getters: {
   },
   mutations: {
     CHANGE_STORE(state, data) {
-      Reflect.set(state.control,data, true)
-
+      Reflect.set(state.control, data, true)
       setTimeout(() => {
-        Reflect.set(state.control,data,  false)
-      },1000)
+        Reflect.set(state.control, data, false)
+      }, 1000)
     }
   },
   actions: {

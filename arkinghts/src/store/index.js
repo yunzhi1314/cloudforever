763ff641ab580a8fea2ae8cb311bbdf1db83f0f1
yxk <3126568631@qq.com>
2023-06-11 20:08:00 +0800

    yxk新增了hooks中的route.js文件和api中的routes.js文件，实现了动态路由的加载

--- arkinghts/src/store/index.js
+++ arkinghts/src/store/index.js
@@ -29,13 +29,35 @@ export default createStore({
       },
       key: "token"
     }),
+    createPersistedState({
+       storage:window.sessionStorage,
+       reducer(state){
+         let baseMsg = {
+           baseRoutes:state.personalCenter.baseRoutes
+         }
+         return isStore(state,'baseRoutes','isBaseRoutes',baseMsg,sessionStorage)
+       },
+       key:'baseRoutes'
+    }),
+    createPersistedState({
+      storage:window.sessionStorage,
+      reducer(state){
+        let baseMsgs = {
+          baseMessage:state.personalCenter.baseMessage
+        }
+        return isStore(state,'baseMessage','isBaseMessage',baseMsgs,sessionStorage)
+      },
+      key:'baseMessage'
+   })
   ],
   state: {
     countDown:10,
     // 控制状态开关集合
     control:{
-      isRegister:false,
-      isLogin:false
+      isRegister:false,//存储注册信息开关
+      isLogin:false,//存储登录信息开关
+      isBaseRoutes:false,//存储路由开关
+      isBaseMessage:false,//存储个人信息开关
     }
   },
   getters: {
---
+++ arkinghts/src/views/center/query/SelfQuery.vue
@@ -0,0 +1,17 @@
+<template>
+                <h1>SelfQuery组件</h1>
+            </template>
+
+            <script>
+
+            export default {
+              name:'SelfQuery',
+                setup() {
+                    return {
+                    }
+                }
+            }
+            </script>
+
+            <style lang="scss" scoped>
+            </style>

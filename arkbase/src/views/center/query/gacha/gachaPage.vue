eae9fccf2066a8535665843f0596372866981c98
liberfly123 <360383409@qq.com>
2023-06-11 11:15:39 +0800

    解决专访记录的分页问题

--- arkbase/src/views/center/query/gacha/gachaPage.vue
+++ arkbase/src/views/center/query/gacha/gachaPage.vue
@@ -17,7 +17,7 @@
             <td>寻访卡池</td>
             <td>获得干员</td>
           </tr>
-          <tr v-for="(item,index) in route.meta.children" :key="index">
+          <tr v-for="(item,index) in arr" :key="index">
             <td> {{ item.time }} </td>
             <td>{{ item.cardPool }}</td>
             <td>{{ item.staff }}</td>
@@ -26,10 +26,9 @@
         <section style="
         font-size: 14px;
         text-align: center;">
-            <span>上一页</span>
-            <span style="margin-left:1vw;margin-right: 0.5vw;color:#409ecd">1</span>
-            <span style="margin-left:0.5vw;margin-right: 1vw;">2</span>
-            <span>下一页</span>
+            <span @click="pageNum--">上一页</span>
+            <span style="margin-left:1vw;margin-right: 0.5vw;">{{ pageNum }}</span>
+            <span @click="pageNum++">下一页</span>
         </section>
         <section style="font-size: 13px;margin:1vh 0vw;">*1.可查询30日、100条以内有效数据，</section>
         <section style="font-size: 13px;">**2.查询结果可能与游戏内实际操作存在延迟。</section>
@@ -42,16 +41,26 @@
 import centerBaseMsgCSS from "@/public/arknights/centerBaseMsg.scss"
 import diamondPageCSS from "@/public/arknights/diamondPage.scss"
 import { useRoute } from "vue-router";
+import { watch,ref } from "vue";
+
 export default {
   name: "gachaPage",
   setup() {
     const route = useRoute()
-    console.log(route.meta)
+    let pageNum = ref(1)
+    let arr = ref({})
+
+    watch(pageNum,newValue=>{
+      arr.value = route.meta.children.slice((newValue-1)*5,newValue*5)
+    },{immediate:true})
+
     return {
       centerBaseMsgCSS,
       diamondPageCSS,
       route,
-      localStorage
+      arr,
+      localStorage,
+      pageNum
     };
   },
 };

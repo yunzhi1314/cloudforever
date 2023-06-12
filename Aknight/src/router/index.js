a63ee2f892c3cb9af04fd6642d79de2ab835d9d2
liaolingyi <1211378806@qq.com>
2023-06-12 09:21:35 +0800

    lly提交重置密码页面和用户信息页面

--- Aknight/src/router/index.js
+++ Aknight/src/router/index.js
@@ -10,6 +10,16 @@ const routes = [
         path:"/center/login",
         name:"loginPage",
         component:()=>import("@/views/center/login/loginPage")
+      },
+      {
+        path:"/center/userinfo",
+        name:"userinfoPage",
+        component:()=> import("@/views/center/login/userinfoPage.vue")
+      },
+      {
+        path:"/center/passwords",
+        name:"resetPasswords",
+        component:()=> import("@/views/center/login/passwordsPage.vue")
       }
     ]
 	}

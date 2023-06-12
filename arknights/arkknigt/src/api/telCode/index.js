0f3650ace7bb7287c9037e9d981454c88911e61f
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-12 11:54:21 +0800

    重新引入了请求库文件

---
+++ arknights/arkknigt/src/api/telCode/index.js
@@ -0,0 +1,18 @@
+import { Request } from "@/hook/request"
+import url from "@/api/url"
+import { reactive } from 'vue'
+
+//获取图形数字验证码 
+ export function telCode(data){
+    let dataList = reactive({
+        code: []
+    })
+
+    // 获取图形验证码
+    Request.post(url.personalCenter.telCode,data)
+    .then(res=>{
+        dataList.code = res.data
+    })
+
+    return dataList
+ } 

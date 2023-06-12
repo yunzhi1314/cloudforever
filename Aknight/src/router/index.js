0251ce50030863d5c0395f3958144cb41a282af6
fatayang <1426122011@qq.com>
2023-06-12 10:10:44 +0800

    fatayang修改了前往数据库组件

--- Aknight/src/router/index.js
+++ Aknight/src/router/index.js
@@ -20,6 +20,10 @@ const routes = [
         ]
       }
     ]
+  },
+  {
+    path:"/database",
+    name:"databasePage"
   }
 ];
 
--- Aknight/src/views/center/to/ToDatabase.vue
+++ Aknight/src/views/center/to/ToDatabase.vue
@@ -1,17 +1,26 @@
 <template>
-                <h1>ToDatabase组件</h1>
-            </template>
+	<div class="toDatabase">
+    <section><h1>前往数据库</h1></section>
+    <section><button @click="toDatabase">点击前往数据库</button></section>
+  </div>
+</template>
 
-            <script>
+<script>
+import {useRouter} from "vue-router"
+	export default {
+		name: "ToDatabase",
+		setup() {
+      let router = useRouter()
+      function toDatabase(){
+        router.push({
+          name:"databasePage",
+        })
+      }
+			return {
+        toDatabase,
+      };
+		}
+	};
+</script>
 
-            export default {
-              name:"ToDatabase",
-                setup() {
-                    return {
-                    }
-                }
-            }
-            </script>
-
-            <style lang="scss" scoped>
-            </style>
+<style lang="scss" scoped></style>

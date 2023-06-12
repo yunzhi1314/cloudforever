c27e5b1ef5d92dd5808416c3d929caa6e2d19c17
braveabc <1302853532@qq.com>
2023-06-13 00:01:10 +0800

    zxy更新了个人清单PersonalList的子页面

---
+++ arkinghts/src/views/center/list/infoBehavior/infoBehaviorPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="infoBox" >
+    <div style="height: 10%;"></div>
+    <div  class="ininfoBasic">
+      <div class="mess" >
+        <section style="border-bottom: 1px solid #E3E3E3;  padding: 0.7vw 0;
+          font-weight: bold; font-size: 14px;">
+          <span style="color: #000;">{{ list.title }}</span>
+        </section>
+        <section>
+          <span>收集场景</span>
+          <span>{{ list.scene }}</span>
+        </section>
+        <section>
+          <span>收集目的</span>
+          <span>{{ list.target }}</span>
+        </section>
+        <section>
+          <span>信息内容及数量</span>
+          <span>{{ list.item }}</span>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { infoBehavior } from "@/api/personalList"
+import { toRefs } from "vue"
+import listcss from "@/public/list.scss"
+
+export default {
+  name: "infoBehaviorPage",
+  setup() {
+
+    return {
+      listcss,
+      ...toRefs(infoBehavior())
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

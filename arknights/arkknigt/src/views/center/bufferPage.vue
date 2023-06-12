b033b6bf447f533fd1d77fceba754b5d263ab226
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-12 11:58:54 +0800

    引入的个人中心文件夹和文件，在toDatabase组件添加了跳转数据库的按钮与方法。

---
+++ arknights/arkknigt/src/views/center/bufferPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>bufferPage.vue组件</h1>
+</template>
+
+<script>
+import { centerMenu } from '@/api/routes';
+export default {
+    name:'bufferPage',
+  setup() {
+   centerMenu()
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

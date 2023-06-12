de808bdec71b8ae787462465bf3b44c94a107657
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-12 11:53:43 +0800

    重新修改的引入的库文件

---
+++ arknights/arkknigt/src/api/mathCode/index.js
@@ -0,0 +1,18 @@
+import url from "@/api/url"
+import { reactive } from 'vue'
+import { Request } from "@/hook/request"
+//获取图形数字验证码 
+ export function mathCode(){
+    let dataList = reactive({
+        code: []
+    })
+
+    // 获取图形验证码
+    Request.get(url.personalCenter.mathCode)
+    .then(res=>{
+        dataList.code = res.data
+    })
+
+    return dataList
+ } 
+

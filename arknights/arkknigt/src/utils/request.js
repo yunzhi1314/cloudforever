dc641ac75eb3b7bc96665adf4558d6e119873624
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-12 11:58:11 +0800

    修改了小bug，重新声明一个变量，请求头设置为token

--- arknights/arkknigt/src/utils/request.js
+++ arknights/arkknigt/src/utils/request.js
@@ -1,35 +1,34 @@
 import axios from "axios";
 import qs from "qs";
-import dev from "../config/dev.env.js";
-import prod from "../config/pro.env.js";
-// 环境的切换
-const env = process.env.NODE_ENV === "development" ? dev : prod;
-// 创建axios实例
+import dev from "../config/dev.env";
+import pro from "../config/pro.env";
+
 const service = axios.create({
-    baseURL: env.BASE_URL, // api的base_url
-    timeout: 10000, // 请求超时时间
+    baseURL: process.env.NODE_ENV == "development" ? dev.API_ROOT : pro.API_ROOT,
+    timeout: 5000
 })
 
-// 设置请求拦截器
 service.interceptors.request.use(config => {
-    // 在发送请求之前做些什么
-    config.data = qs.parse(config.data); // 数据转换
-    config.headers = { // 设置请求头
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    config.data = qs.parse(config.data);
+    config.headers = {
+        "Content-Type": "application/json;charset=utf-8"
     }
+    let vuex = JSON.parse(sessionStorage.getItem('token'));//token权限
+
+    if (vuex) {//如果有token权限的设置请求头为Authorization
+        config.headers['Authorization'] = vuex.token
+    }
+
     return config;
-}, err => {
-    // 抛出错误
-    return Promise.reject(err);
-})
-// 设置响应拦截器
-service.interceptors.response.use(response => {
-    // 对响应数据做点什么
-    if (response.status === 200) {
-        return Promise.resolve(response);
+}, err => Promise.reject(err))
+
+service.interceptors.response.use(config => {
+    if (config.status == 200 || config.status == 204) {
+        return Promise.resolve(config);
     } else {
-        return Promise.reject(response);
+        return Promise.reject(config);
     }
-})
-//导出service
+}
+
+)
 export default service;

46889c2604898f58b732b2bc51000ecd1ab2c61e
braveabc <1302853532@qq.com>
2023-06-12 08:19:40 +0800

    zxy封装了注册和登录的函数，并且新增了吐丝的条件

--- arkinghts/src/hooks/personalCenter/control.js
+++ arkinghts/src/hooks/personalCenter/control.js
@@ -2,8 +2,7 @@ import {reactive} from "vue"
 
 // 全局开关
 let  controlObj = reactive({
-    // 切换注册登录
-    isChange:false,
+    isChange:false,// 切换注册登录
     //遮罩層的开关判断函数
     isDialog:{
        isMathCode:false,//点击获取验证码控制遮罩层的显示
@@ -11,10 +10,10 @@ let  controlObj = reactive({
     isCode:false,
     isRegister:false, //是否是注册
     isLogin:false, //是否是登录,
-    // 吐丝
-    isMsgTusi:false,
+    isMsgTusi:false,// 吐丝
     isPlay:false,//是否动画播放
     isDisable:false,//是否禁用按钮
+    isChecked:false,//是否同意勾选框
 })
 
 export default controlObj
--- arkinghts/src/store/index.js
+++ arkinghts/src/store/index.js
@@ -5,6 +5,16 @@ import { isStore } from '@/hooks/personalCenter/store'
 
 export default createStore({
   plugins: [
+    // 吐丝信息
+    createPersistedState({
+      storage: window.sessionStorage,
+      reducer(state) {
+        return {
+          msg: state.msg
+        }
+      },
+      key: "msg"
+    }),
     createPersistedState({
       storage: window.localStorage,
       reducer(state) {
@@ -13,9 +23,9 @@ export default createStore({
           UID: state.personalCenter.UID,
           email: state.personalCenter.email,
           fullName: state.personalCenter.fullName,
-          userId:state.personalCenter.userId
+          userId: state.personalCenter.userId
         }
-        return isStore(state,"users","isRegister",registerObj,localStorage)
+        return isStore(state, "users", "isRegister", registerObj, localStorage)
       },
       key: "users"
     }),
@@ -24,49 +34,60 @@ export default createStore({
       reducer(state) {
         let tokenData = {
           token: state.personalCenter.token,
+          telephone:state.telephone
         }
-        return isStore(state,"token","isLogin",tokenData,sessionStorage)
+        return isStore(state, "token", "isLogin", tokenData, sessionStorage)
       },
       key: "token"
     }),
     createPersistedState({
-       storage:window.sessionStorage,
-       reducer(state){
-         let baseMsg = {
-           baseRoutes:state.personalCenter.baseRoutes
-         }
-         return isStore(state,'baseRoutes','isBaseRoutes',baseMsg,sessionStorage)
-       },
-       key:'baseRoutes'
+      storage: window.sessionStorage,
+      reducer(state) {
+        let baseMsg = {
+          baseRoutes: state.personalCenter.baseRoutes
+        }
+        return isStore(state, 'baseRoutes', 'isBaseRoutes', baseMsg, sessionStorage)
+      },
+      key: 'baseRoutes'
     }),
     createPersistedState({
-      storage:window.sessionStorage,
-      reducer(state){
+      storage: window.sessionStorage,
+      reducer(state) {
         let baseMsgs = {
-          baseMessage:state.personalCenter.baseMessage
+          baseMessage: state.personalCenter.baseMessage
         }
-        return isStore(state,'baseMessage','isBaseMessage',baseMsgs,sessionStorage)
+        return isStore(state, 'baseMessage', 'isBaseMessage', baseMsgs, sessionStorage)
       },
-      key:'baseMessage'
-   })
+      key: 'baseMessage'
+    })
   ],
   state: {
-    countDown:120,
+    countDown: 120,
+    msg: null,
+    telephone:null,
     // 控制状态开关集合
-    control:{
-      isRegister:false,//存储注册信息开关
-      isLogin:false,//存储登录信息开关
-      isBaseRoutes:false,//存储路由开关
-      isBaseMessage:false,//存储个人信息开关
+    control: {
+      isRegister: false,//存储注册信息开关
+      isLogin: false,//存储登录信息开关
+      isBaseRoutes: false,//存储路由开关
+      isBaseMessage: false,//存储个人信息开关
     }
   },
   getters: {
   },
   mutations: {
-    changeStore(state,name){
-      Reflect.set(state.control,name,true)
+    // 改变存储函数的开关为true
+    changeStore(state, name) {
+      Reflect.set(state.control, name, true)
+    },
+    // 储存提示框信息
+    changeMsg(state, message) {
+      state.msg = message
+    },
+    // 存储电话
+    changeTel(state,data){
+      state.telephone = data
     }
-    
   },
   actions: {
   },
@@ -74,6 +95,6 @@ export default createStore({
   namespaced: true,
   modules: {
     personalCenter
-    
+
   }
 })

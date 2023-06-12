5149f9279e25033f7d4e21fb9e7eaaf8eb632cd3
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-12 11:56:26 +0800

    重新修改了引入的请求库文件

---
+++ arknights/arkknigt/src/hook/personalCenter/control.js
@@ -0,0 +1,18 @@
+import { reactive } from "vue"
+
+// 全局开关
+let controlObj = reactive({
+    // 切换注册登录
+    isChange: false,
+    //遮罩層的开关判断函数
+    isDialog: {
+        isMathCode: false,//点击获取验证码控制遮罩层的显示
+    },
+    isCode: false,
+    isRegister: false, //是否是注册
+    isLogin: false, //是否是登录,
+    isPlay: false,//是否动画播放
+    isDisable: false,//是否禁用按钮
+})
+
+export default controlObj
---
+++ arknights/arkknigt/src/hook/personalCenter/watcher.js
@@ -0,0 +1,19 @@
+import { watch } from "vue";
+
+// 监视账密
+export function watcher(arr) {
+  arr.forEach((item) => {
+    watch(item, (newValue) => {
+      if (item.value == null) {
+        item.isShow = true;
+      }else if(item.zz) {
+        //如果有正则的情况下
+        if (item.zz.test(newValue.value)) {
+          item.isShow = false;
+        } else if (!item.zz.test(newValue.value)) {
+          item.isShow = true;
+        }
+      }
+    });
+  });
+}

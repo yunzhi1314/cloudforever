8a57be3ccd37aa4eb81f5a1de65024a577e74c6b
braveabc <1302853532@qq.com>
2023-06-14 00:52:09 +0800

    zxy更新了设备属性页面渲染

--- arkinghts/src/views/center/list/infoDevice/infoDevicePage.vue
+++ arkinghts/src/views/center/list/infoDevice/infoDevicePage.vue
@@ -1,50 +1,62 @@
 <template>
-  <div style="height: 375vh">
-    <div style="height: 3%"></div>
-    <div class="infoDevice">
-      <section class="title">
-        <span v-for="(item, index) in list" :key="index" @click="changeBox(index)">{{ item.time }}</span>
-      </section>
-      <div v-for="(item, index) in list" :key="index" v-show="index == page">
-        <div v-for="(item2, index2) in item.children" :key="index2">
-          <section style="margin-bottom: 1vh;">{{ item2.title }}</section>
-          <div>
-          <section>
-            <span>收集场景</span>
-            <span>{{ item2.scene }}</span>
-          </section>
-          <section>
-            <span>收集目的</span>
-            <span>{{ item2.target }}</span>
-          </section>
-          <section>
-            <span>信息内容及数量</span>
-            <span>{{ item2.item }}</span>
-          </section>
-        </div>
-        </div>
+<div class="infoBox" >
+    <div style="height: 3%;"></div>
+    <div  class="infoDevice">
+      <div class="time">
+        <section v-for="(item,index) in list" :key="index" @click="showPage(index)"
+        :style="{
+            backgroundColor: index == a ? '#E5F4FB' : '#F5F5F5',
+            border:index == a ? '1px solid #158FC5' : 'none',
+        }"
+        >
+          {{ item.time }}
+        </section>
+      </div>
+      <div v-for="(item2, index2) in children[a]" :key="index2" class="mess">
+        <section 
+        style="
+            background-color: #fff;
+            padding: 1.5vw 0;  
+            font-size: 1rem;
+            font-weight: bold;
+          ">
+        {{ item2.title }}</section>
+        <section>
+          <span>收集场景</span>
+          <span>{{ item2.scene }}</span>
+        </section>
+        <section>
+          <span>收集目的</span>
+          <span>{{ item2.target }}</span>
+        </section>
+        <section>
+          <span>信息内容及数量</span>
+          <span>{{ item2.item }}</span>
+        </section>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { infoDevice } from "@/api/personalList";
-import { toRefs,ref } from "vue";
-import listcss from "@/public/list.scss";
+import { infoDevice } from "@/api/personalList"
+import { toRefs, ref } from "vue"
+import listcss from "@/public/list.scss"
+
 export default {
   name: "infoDevicePage",
   setup() {
-    // 控制渲染那个页面的数字
-    let page = ref('0')
-    function changeBox(index){
-          page.value = index
+    let a = ref(0)
+
+    function showPage(index) {
+      a.value = index 
+      console.log(a.value );
     }
     return {
-      ...toRefs(infoDevice()),
       listcss,
-      changeBox,//改变渲染内容
-      page
+      ...toRefs(infoDevice()),
+      showPage,
+      a
     };
   },
 };

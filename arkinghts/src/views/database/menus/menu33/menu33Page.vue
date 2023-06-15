1fe2a5b13e73972b7f1ec1b6fa28d1ec4d0ddce3
啊啊啊啊 <1302853532@qq.com>
2023-06-15 20:34:26 +0800

    zxy创建了数据库文件夹，并且处理了数据库首页的路由

---
+++ arkinghts/src/views/database/menus/menu33/menu33Page.vue
@@ -0,0 +1,17 @@
+<template>
+                <h1>menu33Page组件</h1>
+            </template>
+
+            <script>
+
+            export default {
+              name:"menu33Page",
+                setup() {
+                    return {
+                    }
+                }
+            }
+            </script>
+
+            <style lang="scss" scoped>
+            </style>

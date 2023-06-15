ddf00686ab7dcab5cf7c8074c11199a91368d8d7
braveabc <1302853532@qq.com>
2023-06-15 23:43:37 +0800

    zxy修护了数据库存储路由为null的Bug

--- arkinghts/src/api/database/layout/index.js
+++ arkinghts/src/api/database/layout/index.js
@@ -11,16 +11,17 @@ export function layout() {
   });
 
   // 数据库左边导航栏路由
-    Request.getData(url.dataBase.layout.menu)
+  Request.getData(url.dataBase.layoutMenu)
     .then((res) => {
       dataList.menu = {...res.data.menu};
-      console.log(dataList.menu);
+      console.log("路由数据",dataList.menu);
 
       // 调用处理路由函数，处理路由路径component
       let fsRoutes = dealpaths(res.data.menu, 0, "component")
-      
+      console.log("处理好的路由",fsRoutes);
+
       // 存储路由信息
-      store.commit("database/changeMenuList",{fsRoutes,menuList:dataList.menu})
+      store.commit("database/changDatabaseMenu",{fsRoutes,menuList:dataList.menu})
       store.commit("changeStore","isDatabase")
 
       // 给路由添加userId,拼接component
@@ -29,7 +30,6 @@ export function layout() {
       // 将子路由添加到databasePage
       routes.forEach( item => {
           router.addRoute("databasePage",item)
-          console.log(item)
       });
     });
 
--- arkinghts/src/api/url.js
+++ arkinghts/src/api/url.js
@@ -35,14 +35,29 @@ export default{
 },
  // 数据库
  dataBase:{
+   
    // 首页左侧菜单
-   layout:{
-       menu:"/database/layout/menu"    
-   },
+   layoutMenu:"/database/layout/menu",   
+
    // homePage数据
    home:{
        messages:"/database/home/messages",
        expMedicals:"/database/home/expMedicals",
+       basicMedical:"/database/home/basicMedical",
+       navTools:"/database/home/navTools",
+       naweiCompany:"/database/home/naweiCompany",
+   },
+   menu:{
+      addMenu:"/database/menu/addMedical",
+      setMenu:"/database/menu/setMedical",
+      delMenu:"/database/menu/delMedical"
+   },
+   role:{
+         addMenu:"/database/role/addMedical",
+         delMenu:"/database/role/delMedical"
+   },
+   user:{
+         queryMedicals:"database/home/expMedical"
    }
 },
 }

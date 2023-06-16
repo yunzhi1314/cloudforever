58650192561de3668a77ad622dcde8ec6c883f48
啊啊啊啊 <1302853532@qq.com>
2023-06-16 20:08:25 +0800

    zxy新增了echarts饼状图的请求

--- arknights/src/echarts/index.js
+++ arknights/src/echarts/index.js
@@ -4,10 +4,12 @@ import url from "@/api/url";
 
 export async function basicMedical(dom) {
     let datas = await Request.getData(url.dataBase.home.basicMedical)
-    console.log(datas);
+    let source = datas.data.datas
+    console.log(source);
+
     let basic = echarts.init(dom)
     let option = {
-
+        
     }
     basic.setOption(option)
 

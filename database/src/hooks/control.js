76d9e186e9a1ab97165bda983c532061bccbc014
方积极 <565205645@qq.com>
2023-06-16 20:10:15 +0800

    fwl在hooks中的control文件新增了登录页面控制按钮

--- database/src/hooks/control.js
+++ database/src/hooks/control.js
@@ -1,12 +1,16 @@
-import {reactive} from "vue"
+import { reactive } from "vue";
 
- let controlObj=reactive({
+let controlObj = reactive({
+	isRegister: false,
+	isCode: false,
+	isFocus: -1,
+	isDialog: {
+		// 登录注册页面
+		isUserCode:false,
+		isAddMenu: false,
+		isAddRole: false,
+		isAddUser: false,
+	},
+});
 
-    isDialog: {
-        isAddMenu:false,
-        isAddRole:false,
-        isAddUser:false,
-    },
- }) 
-
- export default  controlObj
+export default controlObj;

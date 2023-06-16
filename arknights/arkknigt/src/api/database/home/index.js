6ac18afbe7b5c8cd960fafa562a15e1bc19ef538
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-16 10:14:55 +0800

    修改了Request引用的方法

--- arknights/arkknigt/src/api/database/home/index.js
+++ arknights/arkknigt/src/api/database/home/index.js
@@ -1,33 +1,31 @@
 import { onBeforeMount, reactive } from "vue";
-
 import { Request } from "@/hook/request";
-
 export function homeData(url) {
     let dataList = reactive({
         data: [],
     })
 
     onBeforeMount(() => {
-        Request.getData(url)
+        Request.get(url)
             .then((res) => {
                 dataList.data = { ...res.data };
                 console.log(res.data);
             })
-            return dataList
-    })    
+        return dataList
+    })
 }
 
-export function homeData2(url){
+export function homeData2(url) {
     let dataList = reactive({
-        data:[],
+        data: [],
     })
 
     onBeforeMount(() => {
         Request.get(url)
-        .then((res) =>{
-            dataList.data = {...res.data};
-            console.log(res.data);
-        })
+            .then((res) => {
+                dataList.data = { ...res.data };
+                console.log(res.data);
+            })
     })
 
     // 抛出数据

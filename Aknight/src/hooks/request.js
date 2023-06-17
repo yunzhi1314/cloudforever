cfb6ea0bedc27c904ab9360a7a845d6f3c2538bd
fatayang <1426122011@qq.com>
2023-06-17 09:06:09 +0800

    yjb封装了数据库delete请求。

--- Aknight/src/hooks/request.js
+++ Aknight/src/hooks/request.js
@@ -1,16 +1,20 @@
-import req from "@/utils/require"
+import req from "@/utils/require";
 
 export class Request {
-    // get请求
-    static async getData(url){
+	// get请求
+	static async getData(url) {
 		return await req.get(url);
-    }
-    // postqin
-    static async postData(url, data){
-        return await req.post(url, data)
-    }
-    //put请求
-    static async putData(url, data){
-		return await req.put(url, data)
-    }
+	}
+	// postqin
+	static async postData(url, data) {
+		return await req.post(url, data);
+	}
+	//put请求
+	static async putData(url, data) {
+		return await req.put(url, data);
+	}
+	// delete请求可以通过config中的query向后端发送请求数据
+	static async delData(url, config) {
+		return await req.delete(url, config);
+	}
 }

f18b77dceaf3a84a38c54549ad52234b3f50a582
方积极 <565205645@qq.com>
2023-06-18 15:16:58 +0800

    fwl在controlObj主要控制状态文件新增了备注以及登录注册验证码tosi登在登录页面的状态

--- database/src/hooks/control.js
+++ database/src/hooks/control.js
@@ -1,11 +1,19 @@
 import { reactive } from "vue";
 
 let controlObj = reactive({
+	// 登录页面---
+	// 是否注册页面
 	isRegister: false,
+	// 注册前是否已同意协议
+	isCheck:false,
+	// 是否验证码登录
 	isCode: false,
+	// 是否点击input框
 	isFocus: -1,
+	// 是否展示吐司
+	isTosi:false,
 	isDialog: {
-		// 登录注册页面
+		// 登录注册页面遮罩层
 		isUserCode:false,
 		isAddMenu: false,
 		isAddRole: false,

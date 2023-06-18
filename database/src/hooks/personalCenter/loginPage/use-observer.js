17a1bb32e019429a5299f51d5ae68038f839a9c7
方积极 <565205645@qq.com>
2023-06-18 15:16:10 +0800

    fwl修改了watch监听登录框是否符合正则的规则，用于监听确认密码部分

--- database/src/hooks/personalCenter/loginPage/use-observer.js
+++ database/src/hooks/personalCenter/loginPage/use-observer.js
@@ -1,8 +1,15 @@
 import { watch } from "vue";
-export function observer(arr){
+export function observer(arr,index = 1){
+    // 监听是否符合正则以及是否为空
     arr.forEach(item=>{
         watch(item,(newValue)=>{
-            newValue.zz.test(newValue.value) ? newValue.isShow = false : newValue.isShow = true
+            if(newValue.value==null){
+                newValue.isShow = true
+            }else if(newValue.zz){
+                newValue.zz.test(newValue.value) ? newValue.isShow = false : newValue.isShow = true
+            }else{
+                newValue.value == arr[index].value ? newValue.isShow = false : newValue.isShow = true
+            }
         })
     })
 }

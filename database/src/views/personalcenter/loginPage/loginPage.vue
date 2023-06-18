795c00bf2f861e99dd191300dd8bcb37d8c41db0
方积极 <565205645@qq.com>
2023-06-18 15:19:41 +0800

    fwl新增了loginPage页面的验证码部分逻辑事件，新增遮罩层，吐司，登录注册事件

--- database/src/views/personalcenter/loginPage/loginPage.vue
+++ database/src/views/personalcenter/loginPage/loginPage.vue
@@ -1,7 +1,8 @@
 <template>
-	<div class="bg" @click="isFocus = -1">
+	<div class="bg" >
 		<div></div>
-		<div class="login-page" >
+        <!-- 冒泡取消输入框focus样式 -->
+		<div class="login-page" @click="isFocus = -1">
             <!-- 登录框体 -->
 			<div class="login-box" :style="{
                 animation:isRegister? 'toRegister 0.25s 0s 1 ease-in forwards' : 'toLogin 0.25s 0s 1 ease-in forwards'
@@ -12,21 +13,32 @@
                 <div class="main" >
                     <div v-for="(item,index) in userInfo" :key="index" class="main-input">
                         <section>
+                            <!-- 输入框 -->
                             <section @click.stop :class="index == isFocus ? (item.isShow ? 'goodfocus' : 'badfocus') : '' " style="width:100%;">
                                 <input :type="item.type" :placeholder="item.placeholder" v-model="item.value" @focus="toFocus(index)"
                                 :style="{backgroundColor:item.isShow? '#ffdcdb': 'rgba(255, 255, 255, 0)'}"
                                 />
                             </section>
-                            <button v-show="item.isRender" @click="getCode">获取验证码</button>
+                            <!-- 获取验证码 -->
+                            <button v-show="item.isRender" @click="getCode"  
+                            :disabled="(userInfo[0].isShow|| userInfo[0].value == null) || (isCountArr.includes(isRegister?'registerCount' : 'loginCount'))" 
+                            :style="{
+                                backgroundColor:(userInfo[0].isShow|| userInfo[0].value == null) || (isCountArr.includes(isRegister?'registerCount' : 'loginCount')) ?  '#626262' : '#158FC5' ,
+                                cursor:isCountArr.includes(isRegister? 'registerCount' :'loginCount' )? 'wait' : 'pointer'
+                            }">{{isRegister? isCountArr.includes('registerCount') ? registerCount+'s后重试' : '获取验证码' :  isCountArr.includes('loginCount') ? loginCount+'s后重试' : '获取验证码'}}</button>
                         </section>
+                        <!-- 正则提示框 -->
                         <p :style="{visibility:item.isShow? '' : 'hidden'}">{{item.value == '' ? item.tip01 : item.tip02}}</p>
                     </div>
                 </div>
                 <!-- 尾部按钮 -->
                 <div class="footer">
+                    <!-- 登录页面切换登录方式 -->
                     <section v-show="!isRegister" @click="toCode">{{isCode? '使用密码登录':'使用短信验证码登录'}}</section>
+                    <!-- 注册页面同意提示 -->
                     <div v-show="isRegister" class="agreement">
-                        <input type="checkbox">
+                        <!-- 注册同意协议按钮点击取反 -->
+                        <input type="checkbox"  :checked="isCheck" @click="isCheck = !isCheck">
                         <section>
                             <span>已阅读并同意</span>
                             <a href="">《鹰角网络用户注册协议》</a>
@@ -34,7 +46,8 @@
                             <a href="">《鹰角网络游戏个人信息保护政策》</a>
                         </section>
                     </div>
-                    <button>{{isRegister?'注册' : '登录'}}</button>
+                    <!-- 登录注册框按钮 -->
+                    <button @click="useUser">{{isRegister?'注册' : '登录'}}</button>
                     <section v-show="!isRegister">使用Bilibili账号</section>
                 </div>
             </div>
@@ -62,14 +75,23 @@
 			</div>
 		</div>
 		<div></div>
-
 	</div>
 
+    <!-- 遮罩层 -->
     <dialogPage>
         <div class="get-Code" @click.stop>
-
+            <!-- 验证码图片 -->
+            <section v-html="svg" @click="getSvg"></section>
+            <!-- 输入框 -->
+            <section><input type="text" v-model="codeData.mathCode"></section>
+            <!-- 按钮区 -->
+            <section><button @click="onCountdown(isRegister?'registerCount' : 'loginCount')">确认</button><button @click="outDialog">取消</button></section>
         </div>
     </dialogPage>
+
+    <!-- 吐司 -->
+    <TosiPage v-if="isTosi">
+    </TosiPage>
 </template>
 
 <script>
@@ -78,13 +100,18 @@ import CodeSCSS from '@/public/getCode.scss';
 import loginSCSS from '@/public/personalCenter/loginPage.scss';
 import { changeBoard, toCode, toFocus } from "@/hooks/personalCenter/loginPage/use-changeBoard";
 import userInfo from '@/hooks/personalCenter/loginPage/userInfo';
-import {getCode} from '@/hooks/personalCenter/loginPage/use-code'
+import {getCode, getSvg, outDialog, colck,codeBtn} from '@/hooks/personalCenter/loginPage/use-code'
 import svg from '@/hooks/personalCenter/loginPage/use-code'
+import {useUser} from '@/hooks/personalCenter/loginPage/use-User'
 import { toRefs, provide } from "vue";
 export default {
 	name: "loginPage",
 	setup() {
+        // 传递遮罩层状态
         provide('name','isUserCode')
+        // 调用并结构倒计时内容
+        let {codeData,onCountdown} = codeBtn('isUserCode')
+
 		return {
             // CSS
             loginSCSS,
@@ -95,9 +122,25 @@ export default {
 			changeBoard,
             // 渲染数组
             userInfo,
+            // 点击验证码登录事件
             toCode,
+            // 点击input框触发动画
             toFocus,
+            // 点击获取验证码
             getCode,
+            // 验证码svg图
+            svg,
+            // 重新获取验证码svg事件
+            getSvg,
+            // 关闭遮罩层
+            outDialog,
+            codeData,
+            // 点击开启倒计时事件
+            onCountdown,
+            // 拓展倒计时输入框
+            ...toRefs(colck),
+            // 登录注册事件
+            useUser,
 		};
 	},
 };

f50655e9091c23b988aa01db61859097af6742ec
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-23 17:03:03 +0800

    添加了表单查询的按钮判断

--- arknights/arkknigt/src/hook/personalCenter/control.js
+++ arknights/arkknigt/src/hook/personalCenter/control.js
@@ -7,6 +7,8 @@ let controlObj = reactive({
     //遮罩層的开关判断函数
     isDialog: {
         isMathCode: false,//点击获取验证码控制遮罩层的显示
+        isAddMenu:false, //控制新增菜单的遮罩层  *yzy*
+     
     },
     isCode: false,
     isRegister: false, //是否是注册

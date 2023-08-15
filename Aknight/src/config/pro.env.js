791fa39bc40c7343b49021074b2a9bc274894927
yzy <1375223150@qq.com>
2023-08-15 15:24:35 +0800

    修复了请求引入问题 添加了请求配置文件和开发生产环境位置   的项目框架

--- Aknight/src/config/pro.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    NODE_ENV:"production",
-    BASE_URL:"http://8.134.179.237:8080/"
-}
--- Aknight/src/hooks/toset.js
+++
@@ -1,9 +0,0 @@
-// 封装吐丝
-export function toest(controlObj) {
-	setTimeout(() => {
-		controlObj.isTusi = true;
-	}, 250);
-	setTimeout(() => {
-		controlObj.isTusi = false;
-	}, 2000);
-}

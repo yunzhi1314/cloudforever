69f346e14a3837f2d6465f8c9c182437e8d3082c
2488627878 <2488627878@qq.com>
2023-08-15 16:00:23 +0800

    JX去除不需要内容

--- yuansheng/src/router/index.ts
+++ yuansheng/src/router/index.ts
@@ -1,6 +1,12 @@
-import { createRouter, createWebHistory,RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const routes:Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path:"/",
+    name:"database",
+    component:()=>'@/Projects/database/index.vue'
+  }
+];
 
 const router = createRouter({
   history: createWebHistory('/geshin/'),

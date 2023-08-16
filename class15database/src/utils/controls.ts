f01aa6fa4b5dc8de70baf059aa1085a35feb1bf1
merry00 <1215302878@qq.com>
2023-08-16 16:33:40 +0800

    毛耀弘做 来了menu页面和删除弹框

--- class15database/src/utils/controls.ts
+++ class15database/src/utils/controls.ts
@@ -1,7 +1,10 @@
 import {reactive} from "vue"
 
 const controlObj = reactive({
-  
+//   弹框 菜单 确认删除信息
+menuDeletFlag:false,
+//   弹框 菜单 添加；列表信息
+  menuAddFlag:false,
 })
 
 export default controlObj

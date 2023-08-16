99609c253384dd61fe7e98b28bb903e0be605b44
linwenzuishuai <778999823@qq.com>
2023-08-16 19:16:08 +0800

    linwen resolve code

--- class15database/vite.config.ts
+++ class15database/vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),vueJsx()],
-  base:"/database/",
+  base:"/geshin/",
   define:{
     'process.env':{}
   },

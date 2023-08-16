11de547ac06914c66705970f72dee85b24ec1226
yzy <1375223150@qq.com>
2023-08-16 10:28:38 +0800

    yzy 封装了get 请求函数

--- myvueprject/src/utils/require.ts
+++ myvueprject/src/utils/require.ts
@@ -67,3 +67,21 @@ export function DOMDataObj(url:string,method:keyof Method,data:Data,propName:str
 
    return dataList
 }
+
+// 自己添加的get 请求   
+export function getData(url:string,method:string,data:any){
+    let dataList = reactive({
+        animation:"",  //动画界面
+        public:"",
+        page1:"",  
+        page2:"",
+        getCode:"", //图形验证码
+    })
+    req.get(url,data).then((res:any)=>{
+      url == "/geshin/animation" ? dataList.animation =  res.data :
+        url == "/geshin/public" ? dataList.public =  res.data : 
+         url == "/geshin/index/page1" ? dataList.page1 =  res.data: 
+         url == "/geshin/index/page2" ?  dataList.page2  =  res.data:
+         url == " /getCode"  ? dataList.getCode = res.data  :""
+    })
+}

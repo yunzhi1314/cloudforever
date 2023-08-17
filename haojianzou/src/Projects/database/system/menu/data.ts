8f6dac19bcc2a5dbd99319eb3d609a877ae57b88
merry00 <1215302878@qq.com>
2023-08-17 19:15:01 +0800

    更新

--- haojianzou/src/Projects/database/system/menu/data.ts
+++ haojianzou/src/Projects/database/system/menu/data.ts
@@ -1,5 +1,6 @@
 import {reactive } from "vue"
-
+import  req from "@/utils/request"
+import { dataTool } from "echarts";
 
 interface menuObj{
     CompanyName:string;
@@ -8,10 +9,26 @@ interface menuObj{
     NumberOfLines: string;
     TreatmentMethod: string;
     Area: string;
+    children?:any;
 }
 
 export let titleObj=reactive<menuObj>({CompanyName:'所属公司',medication:'药物名称',
 Target:'靶向', NumberOfLines:'线数',TreatmentMethod:'治疗方式',Area:'区域'  }) 
+
+// 请求菜单数据
+export function reqMenuData(){
+    req.get("/database/home/basicMedical").then((res:any)=> {
+        if(res.status==200){
+            console.log(res.data.datas,'1111')
+            return res.data.datas
+        }
+    // if(res.data.token){
+    //     localStorage.setItem("token",res.data.token)
+    //     localStorage.setItem("userid",res.data.userId)
+    // } }) -,/medical医药，前面的/home 
+})}
+
+
 
 
 
@@ -32,6 +49,24 @@ export let objList=reactive<Array< menuObj>>([
                         NumberOfLines: "GC1L",
                         TreatmentMethod: "化疗",
                         Area: "国际",
+                        children:[
+                            {
+                                CompanyName:"基石药业",
+                                medication: "帕博利珠单抗",
+                                Target: 'PD-1',
+                                NumberOfLines: "GC1L",
+                                TreatmentMethod: "化疗",
+                                Area: "国际",
+                            },
+                            {
+                                CompanyName:"基石药业",
+                                medication: "舒格利单抗",
+                                Target: 'PD-1',
+                                NumberOfLines: "GC1L",
+                                TreatmentMethod: "化疗",
+                                Area: "国际",
+                            },
+                        ]
                     },
                     {
                         CompanyName:"方生物",

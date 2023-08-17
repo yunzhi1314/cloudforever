65116635902b695e3c0e16263079c9946a2b482a
1Newfolder <2647049796@qq.com>
2023-08-17 14:10:06 +0800

    优化版本

--- yuansheng/src/Projects/database/system/user/userPage.vue
+++ yuansheng/src/Projects/database/system/user/userPage.vue
@@ -5,68 +5,18 @@
     <!-- 增查输入框 -->
     <div class="header" style="margin-left:10px">
         <el-input v-model="input" placeholder="请输入查询内容" class="input" />
-        <el-button type="primary" style="margin-left:10px" @click="fun"><el-icon><Search /></el-icon>查询</el-button>
+        <el-button type="primary" style="margin-left:10px" ><el-icon><Search /></el-icon>查询</el-button>
         <el-button type="success"><el-icon><FolderAdd /></el-icon>新建菜单</el-button>
     </div>
 
-    <!-- 表格 -->
-<el-table :data="tableData()" style="width: 100% ;height:70vh">
-
-  <el-table-column  label="药物名称">
-    <template #default="scope">
-    <div style="display: flex; align-items: center">
-      <span>{{ scope.row.name }}</span>
-    </div>
-  </template>
-  </el-table-column>
-
-      <el-table-column prop="schedule" label="研发进度">
-        <template #default="scope">
-      <el-progress :text-inside="true" :stroke-width="26" :percentage="scope.row.schedule" />
-  </template>
-      </el-table-column>
-
-      <el-table-column prop="china" label="中国入组人数">
-        <template #default="scope">
-    <div style="display: flex; align-items: center">
-      <span>{{ scope.row.china }}</span>
-    </div>
-  </template>
-      </el-table-column>
-
-      <el-table-column prop="int" label="国际入组人数">
-        <template #default="scope">
-    <div style="display: flex; align-items: center">
-      <span>{{ scope.row.int }}</span>
-    </div>
-  </template> 
-      </el-table-column>
-
-      <el-table-column prop="deta" label="首次公开日期">
-        <template #default="scope">
-    <div style="display: flex; align-items: center">
-      <span>{{ scope.row.deta }}</span>
-    </div>
-  </template>
-      </el-table-column>
-
-      <el-table-column prop="register" label="中国FPI注册">
-        <template #default="scope">
-    <div style="display: flex; align-items: center">
-      <span>{{ scope.row.register }}</span>
-    </div>
-  </template>
-      </el-table-column>
-
-      <el-table-column prop="aut" label="美国CTR认证">
-        <template #default="scope">
-    <div style="display: flex; align-items: center">
-      <span>{{ scope.row.aut }}</span>
-    </div>
-  </template>
-      </el-table-column>
-
-      <el-table-column label="操纵">
+<!-- 表格 -->
+<el-table :data="tableData()" height="510" >
+  <el-table-column v-for="(item, index) in arr" :key="index" :prop="arr2[index]" :label="item" >
+        <template #default="scope" >
+           <el-progress :text-inside="true" :stroke-width="26" :percentage="scope.row.schedule" v-if = "index == 1"/>
+        </template>
+    </el-table-column>
+  <el-table-column label="操纵">
         <template #default="scope">
           <el-button size="small" @click="handleEdit(scope.$index, scope.row)">修改</el-button>
           <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
@@ -76,9 +26,7 @@
 
 <!-- 分页 -->
 <div class="example-pagination-block">
-  <!-- <div class="example-demonstration">分页</div> -->
   <el-pagination
-    background
     layout="sizes, prev, pager, next, jumper,total"
     :total="state.total"
     :page-sizes="[5, 10, 15, 20]"
@@ -93,9 +41,9 @@
 <script setup lang="ts">
 
 // 第三版
-import { defineComponent, reactive, toRefs, ref } from "vue";
+import {  reactive, ref } from "vue";
 
-const input = ref("")
+let input = ref("")
 
 interface data {
 name: string,
@@ -106,8 +54,20 @@ deta:string,
 register:string,
 aut:string,
 }
+
+// 将头部变为数组渲染
+let arr = [
+  '药物名称',
+  '研发进度',
+  '中国入组人数',
+  '国际入组人数',
+  '首次公开日期',
+  '中国FPI注册',
+  '美国CTR认证',
+]
+
 //表格的全数据（这里是自定义的列表，要看分页效果自行往此数组内加数据）
-const allTableData:Array<data> = reactive([
+let allTableData = reactive([
 {
 name: '卡瑞立柱单抗',
 schedule: '100',
@@ -234,40 +194,44 @@ deta:'202-04-21',
 register:'2020-09-21',
 aut:'CTR20200708',
 },
-
 ]);
 
+let arr2 = Reflect.ownKeys(allTableData[0]) as Array<string>
+  console.log(arr2)
+
 //表格用到的参数
-const state = reactive({
+let state = reactive({
   page: 1,
   limit: 10,
   total: allTableData.length,
 });
 
+
 //前端限制分页（tableData为当前展示页表格）
-const tableData = () => {
+let tableData = () => {
   return allTableData.filter(
     (item, index) =>
       index < state.page * state.limit &&
       index >= state.limit * (state.page - 1),
   );
 };
+tableData()
 
 //改变页码
-const handleCurrentChange = (val:any) => {
+let handleCurrentChange = (val:any) => {
   state.page = val;
 };
 
 //改变页数限制
-const handleSizeChange = (val:any) => {
+let handleSizeChange = (val:any) => {
   state.limit = val;
 };
 
 // 表格的增删
-const handleEdit = (index: number, row: data) => {
+let handleEdit = (index: number, row: data) => {
 console.log(index, row)
 }
-const handleDelete = (index: number, row: data) => {
+let handleDelete = (index: number, row: data) => {
 allTableData.splice(index,1)
 console.log(index, row)
 }

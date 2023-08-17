a4d90925a93a786fc1d16d80b1a9594c66809bfa
yzy <1375223150@qq.com>
2023-08-17 10:31:26 +0800

    yzy 提交了一个测试文件

--- myvueprject/src/Projects/geshin/spots/index.vue
+++ myvueprject/src/Projects/geshin/spots/index.vue
@@ -25,43 +25,7 @@
        </div>
         </main>
         </div>
-    </template>
-  <div class="spots">
-    <!-- 米游社头部 -->
-    <header class="spots_header">
-      <section class="header_left">
-        <img src="" alt="" />
-        <span>米游社</span>
-        <span>|</span>
-        <span>米游社·原神</span>
-        <el-icon><ArrowUpBold /></el-icon>
-      </section>
-      <section>
-        <nav>
-          <li></li>
-          <span>更多</span>
-          <el-icon><ArrowDownBold /></el-icon>
-        </nav>
-      </section>
-      <section>
-        <!-- <el-button size="large" :icon="Search" round>Search</el-button> -->
-        <div>
-          <el-avatar
-            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-          />
-        </div>
-      </section>
-    </header>
-    <!-- 内容 -->
-    <main>
-
-    </main>
-    <!-- 底部 -->
-    <footer>
-
-    </footer>
-  </div>
-</template>
+  </template>
 
     <script setup lang="ts">
 import { reactive } from "vue";

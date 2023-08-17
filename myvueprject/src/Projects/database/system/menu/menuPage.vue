b097277bdc597324cd02f6209c57768bc794a646
2488627878 <2488627878@qq.com>
2023-08-17 15:50:27 +0800

    111

--- myvueprject/src/Projects/database/system/menu/menuPage.vue
+++ myvueprject/src/Projects/database/system/menu/menuPage.vue
@@ -1,14 +1,26 @@
 <template>
- <div>
-  <h1>menuPage.vue</h1>
- </div>
+    <div style="width: 100%;height: 100%;">
+        <el-row>
+            <el-input v-model="search" style="width: 14vw;" placeholder="请输入查询内容" :prefix-icon="Search" />
+            <el-button type="primary" style="width: 5vw;height: 4vh;margin-left: 1vw;" :icon="Search">查询</el-button>
+            <el-button type="success" style="width: 7vw;height: 4vh;"><el-icon>
+                    <FolderAdd />
+                </el-icon>新增菜单</el-button>
+        </el-row>
+        <el-row>
+            <el-table :data="tableData" style="width: 100%" height="250">
+                <el-table-column fixed prop="date" label="Date" width="150" />
+
+            </el-table>
+        </el-row>
+    </div>
 </template>
 
 <script setup lang="ts">
-
-
+import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+let search = ref("")
 
 </script>
-<style lang="scss" scoped>
 
-</style>
+<style scoped lang="scss"></style>

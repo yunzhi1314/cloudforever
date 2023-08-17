5d5adf5a7a7b26e534df9bfeb7f167dc7d8225b3
wuhuaxing <wuhuaxing@qq.com>
2023-08-17 14:56:08 +0800

    吴华兴的提交

---
+++ vue3-mvp/vue3-mvp/src/Projects/geshin/roles/character.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in citys" :key="item" @click="changeRouter(item)" class="active">{{ item }}</li>
+            <li>敬请期待</li>
+        </ul>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue"
+import { useRouter } from "vue-router";
+
+let $router = useRouter()
+
+let citys = reactive([
+    '蒙德城', '璃月港', '稻妻城', '须弥城', '枫丹延'
+])
+
+function changeRouter(item: string) {
+    $router.push({
+        name: item,
+        params: {
+            city: item
+        }
+    })
+}
+
+</script>
+
+<style scoped lang="scss">
+ul {
+    position: absolute;
+    color: #fff;
+    font-size: 18px;
+    top: 187px;
+    line-height: 54px;
+
+    li {
+        background-repeat: no-repeat;
+        width: 370px;
+        height: 54px;
+        padding-left: 70px;
+        background-repeat: no-repeat;
+    }
+
+    .active {
+        background: url(../../../assets/下载.png);
+    }
+}
+</style>
--- vue3-mvp/vue3-mvp/src/Projects/geshin/roles/layout.vue
+++ vue3-mvp/vue3-mvp/src/Projects/geshin/roles/layout.vue
@@ -2,14 +2,6 @@
     <div class="main">
         <div class="background">
             <span class="left-white"></span>
-            <ul>
-                <li @click="redirect('mond')" class="active">蒙德城</li>
-                <li @click="redirect('liyue')">璃月港</li>
-                <li @click="redirect('ricewife')">稻妻城</li>
-                <li @click="redirect('sumeru')">须弥城</li>
-                <li @click="redirect('fengdanyan')">枫丹延</li>
-                <li>敬请期待</li>
-            </ul>
             <div class="main-body">
                 <span></span>
                 <img src="../../../assets/name.png" alt="">
@@ -31,22 +23,19 @@
                 </div>
             </div>
             <div class="Carousel">
+                <div class="left"></div>
+                <div>
 
+                    <p></p>
+                </div>
+                <div class="right"></div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-
-const router = useRouter()
-
-
-function redirect(routeName: any) {
-    router.replace({ name: routeName })
-}
 
 const cvText = ref('林簌')
 
@@ -61,11 +50,12 @@ function switchCVLanguage() {
   
 <style scoped lang="scss">
 .main {
-    width: 100vw;
-    height: 100vh;
+    width: 1349px;
+    height: 800px;
+
     .background {
-        width: 100vw;
-        height: 100vh;
+        width: 1349px;
+        height: 800px;
         background: url(../../../assets/one.png);
         background-repeat: no-repeat;
         background-size: 100vw 100vh;
@@ -76,26 +66,6 @@ function switchCVLanguage() {
             position: absolute;
             left: 38px;
             background-color: #C0C0C0;
-        }
-
-        ul {
-            position: absolute;
-            color: #fff;
-            font-size: 18px;
-            top: 187px;
-            line-height: 54px;
-
-            li {
-                background-repeat: no-repeat;
-                width: 370px;
-                height: 54px;
-                padding-left: 70px;
-                background-repeat: no-repeat;
-            }
-
-            .active {
-                background: url(../../../assets/下载.png);
-            }
         }
 
         .main-body {
@@ -163,6 +133,9 @@ function switchCVLanguage() {
         .Carousel {
             width: 100%;
             height: 190px;
+            position: relative;
+            top: 610px;
+            background: rgba(204, 204, 204, 0.2);
         }
     }
 }

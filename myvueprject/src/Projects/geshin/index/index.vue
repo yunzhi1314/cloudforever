60a6dd4d811da2e3d3f3412c19935012498afa9e
shanghaoqingnian <1805562803@qq.com>
2023-08-18 09:20:50 +0800

    小莫，首页视频部分已经弄上去

--- myvueprject/src/Projects/geshin/index/index.vue
+++ myvueprject/src/Projects/geshin/index/index.vue
@@ -1,27 +1,43 @@
 <template>
-    <div>
-      <!-- 中间顶部 -->
-      <div>
-        
-      </div>
-
-      <!-- 中间底部 -->
-      <div>
-         
-      </div>
-
-      <video></video>
-    </div>
+  <div>
+    <div></div>
+    <div></div>
+    <video :src="videoSrc" autoplay muted style="width: 98.7vw;"></video>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { Numlist } from "@/utils/require";
+import { ref, onMounted } from "vue";
+import req from "@/utils/request"; // 导入你的网络请求实例
 
-// req.get().then
+const videoSrc = ref("");
+
+onMounted(async () => {
+    const response = await req.get("/api/geshin/index/page1");
+    videoSrc.value = response.data.dataList.background.bgVideo;
+    // 获取视频元素并播放
+    const videoElement = document.querySelector("video") as HTMLVideoElement;
+    if (videoElement) {
+      videoElement.play();
+    }
+});
 
 
 </script>
-    
+
 <style scoped>
-    
+/* 隐藏视频控制条 */
+video::-webkit-media-controls {
+  display: none;
+}
+video::-webkit-media-controls-enclosure {
+  display: none !important;
+}
+video::-webkit-media-controls-overlay-play-button {
+  display: none !important;
+}
 </style>
+
+
+
+
